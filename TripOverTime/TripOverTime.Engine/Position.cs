98505a4e6b8cfa8c032d79b9d662179da21c7d0c
Sourciluss667 <qcormand@gmail.com>
2019-10-22 10:46:30 +0200

    Add Position.cs

---
+++ TripOverTime/TripOverTime.Engine/Position.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TripOverTime.Engine
+{
+    class Position
+    {
+        double _x;
+        double _y;
+
+        /// <summary>
+        /// Init position
+        /// </summary>
+        /// <param name="x">Default 0</param>
+        /// <param name="y">Default 0</param>
+        public Position(double x = 0, double y = 0)
+        {
+            _x = x;
+            _y = y;
+        }
+
+        public double X
+        {
+            get => _x;
+            internal set => _x = value;
+        }
+        public double Y
+        {
+            get => _y;
+            internal set => _y = value;
+        }
+    }
+}
