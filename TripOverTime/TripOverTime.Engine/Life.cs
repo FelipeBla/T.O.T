a1091606444d174a0b88e89c3b587ce5831350bb
Florian <florian.nchoh@outlook.fr>
2019-10-31 10:06:18 +0100

    Completed Life

--- TripOverTime/TripOverTime.Engine/Life.cs
+++ TripOverTime/TripOverTime.Engine/Life.cs
@@ -4,7 +4,93 @@ using System.Text;
 
 namespace TripOverTime.EngineNamespace
 {
-    class Life
+    //
+    public class Life
     {
+        readonly Game _context;
+        Monster _monster;
+        Player _player;
+        readonly ushort _maxPoint;
+        readonly ushort _minPoint;
+        ushort _currentPoint;
+        ushort _damage;
+
+        public Life()
+            : this(1000, 0)
+        {
+        }
+
+        public Life(ushort maxPoint, ushort minPoint)
+        {
+            _maxPoint = maxPoint;
+            _minPoint = minPoint;
+        }
+
+        public ushort CurrentPoint
+        {
+            get { return _currentPoint; }
+        }
+
+        public bool IsFull
+        {
+            get { return _currentPoint == _maxPoint; }
+        }
+
+        public bool IsEmpty
+        {
+            get { return _currentPoint == 0; }
+        }
+
+        public void Empty(ushort point)
+        {
+            if (point > _currentPoint) throw new InvalidOperationException("The amount of point to remove is to big.");
+
+            _currentPoint -= point;
+        }
+
+        public void Empty()
+        {
+            _currentPoint = _minPoint;
+            _currentPoint = 0;
+        }
+
+        public void Fill(ushort point)
+        {
+            if (_currentPoint + point > _maxPoint) throw new InvalidOperationException("The amount of point to add is to big.");
+
+            _currentPoint += point;
+        }
+
+        public void Fill()
+        {
+            _currentPoint = _maxPoint;
+            _currentPoint = 1000;
+        }
+
+        public ushort GetCurrentPoint()
+        {
+            return _currentPoint;
+        }
+
+        public ushort GetMinPoint()
+        {
+            return _maxPoint;
+        }
+        public ushort GetMaxPoint()
+        {
+            return _maxPoint;
+        }
+
+        public void DecreasedPoint(ushort damage)
+        {
+            if (damage < 0 || damage > _currentPoint) return;
+
+            _currentPoint -= damage;
+        }
+
+        public void BonusPoint(ushort bonus)
+        {
+            _currentPoint += Convert.ToUInt16(Math.Max(Convert.ToInt32(bonus), 0));
+        }
     }
 }
