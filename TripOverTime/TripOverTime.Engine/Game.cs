11bf83e70e63ac9ba97fc4c0e7f5eb1984c4bdaa
Sourciluss667 <qcormand@gmail.com>
2019-12-10 16:13:42 +0100

    Win MENU

--- TripOverTime/TripOverTime.Engine/Game.cs
+++ TripOverTime/TripOverTime.Engine/Game.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Text;
 
 namespace TripOverTime.EngineNamespace
@@ -10,6 +11,7 @@ namespace TripOverTime.EngineNamespace
         Player _player;
         List<Monster> _monsters;
         Map _map;
+        Stopwatch _timer;
 
         internal Game(Engine context, string mapPath, string playerPath, Position startPosition)
         {
@@ -17,6 +19,8 @@ namespace TripOverTime.EngineNamespace
             _map = new Map(this, mapPath);
             _player = new Player(this, "player", startPosition, new Life(100, 1), 5, playerPath);
             _monsters = _map.GenerateMonsters();
+            _timer = new Stopwatch();
+            _timer.Start();
         }
 
         internal Map GetMapObject
@@ -32,6 +36,10 @@ namespace TripOverTime.EngineNamespace
             get => _context;
         }
 
+        internal long TimeElapsed
+        {
+            get => _timer.ElapsedMilliseconds;
+        }
         internal List<Monster> GetMonsters
         {
             get => _monsters;
